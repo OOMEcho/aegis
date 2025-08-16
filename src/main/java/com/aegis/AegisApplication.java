1d9742e30d0f4513d5e96ad910249981315add30
xuesong.lei <228389787@qq.com>
2025-08-16 17:17:26 +0800

    feat: 添加mapper注解

--- src/main/java/com/aegis/AegisApplication.java
+++ src/main/java/com/aegis/AegisApplication.java
@@ -1,8 +1,10 @@
 package com.aegis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.aegis.**.mapper")
 @SpringBootApplication
 public class AegisApplication {
 
