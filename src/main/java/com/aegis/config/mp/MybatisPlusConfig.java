b7cfbd7459e8160cc56a954eb893fa0dac588857
xuesong.lei <228389787@qq.com>
2025-08-23 19:28:49 +0800

    feat: 增加数据库示例表、添加乐观锁插件

--- src/main/java/com/aegis/config/mp/MybatisPlusConfig.java
+++ src/main/java/com/aegis/config/mp/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.aegis.config.mp;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
---
+++ src/main/resources/script/default.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `t_table_name`;
+CREATE TABLE `t_table_name`
+(
+    `id`          BIGINT UNSIGNED AUTO_INCREMENT COMMENT '主键ID',
+    `create_by`   VARCHAR(64)         DEFAULT NULL COMMENT '创建人',
+    `update_by`   VARCHAR(64)         DEFAULT NULL COMMENT '更新人',
+    `create_time` DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` DATETIME   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`     TINYINT(1) NOT NULL DEFAULT 0 COMMENT '逻辑删除标记 0=正常 1=删除',
+    `version`     INT        NOT NULL DEFAULT 1 COMMENT '版本号，用于乐观锁',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='示例表';
