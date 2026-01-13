5140b9de0752255a2bb69c3cb742a284c135cf0f
xuesong.lei <228389787@qq.com>
2026-01-13 10:24:47 +0800

    fix：优化授权逻辑

--- src/main/java/com/aegis/config/security/handler/MyAccessDecisionManager.java
+++ src/main/java/com/aegis/config/security/handler/MyAccessDecisionManager.java
@@ -1,6 +1,7 @@
 package com.aegis.config.security.handler;
 
 import com.aegis.common.constant.CommonConstants;
+import com.aegis.common.exception.LoginException;
 import com.aegis.common.result.ResultCodeEnum;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
@@ -26,6 +27,9 @@ public class MyAccessDecisionManager implements AccessDecisionManager {
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
         // 检查是否有NONE权限
         if (configAttributes.stream().anyMatch(attr -> CommonConstants.NONE.equals(attr.getAttribute()))) {
+            if (authentication == null || !authentication.isAuthenticated()) {
+                throw new LoginException(ResultCodeEnum.NOT_LOGGED_IN);
+            }
             return;
         }
 
