a0aed790463a541e5f2050c505ba3cb1062343a0
xuesong.lei <228389787@qq.com>
2026-02-02 15:33:51 +0800

    feat: 添加仪表板统计功能和访问趋势数据接口

---
+++ src/main/java/com/aegis/modules/common/domain/vo/DashboardVO.java
@@ -0,0 +1,39 @@
+package com.aegis.modules.common.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Author: xuesong.lei
+ * @Date: 2026/2/2
+ * @Description: Dashboard统计VO
+ */
+@Data
+@ApiModel("Dashboard统计VO")
+public class DashboardVO {
+
+    @ApiModelProperty("用户总数")
+    private Long userCount;
+
+    @ApiModelProperty("用户近7日增长百分比")
+    private String userGrowthRate;
+
+    @ApiModelProperty("角色总数")
+    private Long roleCount;
+
+    @ApiModelProperty("角色近7日增长百分比")
+    private String roleGrowthRate;
+
+    @ApiModelProperty("权限总数")
+    private Long permissionCount;
+
+    @ApiModelProperty("权限近7日增长百分比")
+    private String permissionGrowthRate;
+
+    @ApiModelProperty("通知总数")
+    private Long noticeCount;
+
+    @ApiModelProperty("通知近7日增长百分比")
+    private String noticeGrowthRate;
+}
