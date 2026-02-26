5b815ffcfee72c7affec86dfc7da8b8dc9fefcdc
xuesong.lei <228389787@qq.com>
2026-02-26 17:20:07 +0800

    feat: 增强文件存储服务，添加目录参数校验和清洗功能

--- src/main/java/com/aegis/modules/role/domain/dto/RoleDTO.java
+++ src/main/java/com/aegis/modules/role/domain/dto/RoleDTO.java
@@ -33,8 +33,8 @@ public class RoleDTO extends PageDTO {
     private String remark;
 
     @ApiModelProperty("角色名称")
-    @NotBlank(groups = {ValidGroup.Create.class, ValidGroup.Update.class}, message = "菜单名称不能为空")
-    @Size(groups = {ValidGroup.Create.class, ValidGroup.Update.class}, max = 32, message = "菜单名称长度不能超过32个字符")
+    @NotBlank(groups = {ValidGroup.Create.class, ValidGroup.Update.class}, message = "角色名称不能为空")
+    @Size(groups = {ValidGroup.Create.class, ValidGroup.Update.class}, max = 32, message = "角色名称长度不能超过32个字符")
     private String roleName;
 
     @ApiModelProperty("角色编码")
